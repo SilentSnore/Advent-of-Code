9579d801f69705bc95bee2e691bfdf7ce807daaa
Dominik Pezelj <dominik.pezelj@outlook.com>
2017-12-03 22:24:35 +0100

    Day3 done

--- Advent of Code/Day3/Program.cs
+++ Advent of Code/Day3/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day3
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int n = 325489;
@@ -32,9 +29,73 @@ namespace Day3
             int udaljenost = u + uu - 1;
             Console.WriteLine(udaljenost);
 
-
-
-            Console.ReadKey();
+            //drugi
+            int[,] matrix = new int[503, 503];
+            int number = 1;
+            matrix[252, 252] = number;
+            int desniRub = 252;
+            int gornjiRub = 252;
+            int lijeviRub = 252;
+            int donjiRub = 252;
+            int duljina = 1;
+            int vrati = 0;
+            while (true)
+            {
+                //pogled s desne
+                for (int i = vrati; i <= duljina; i++)
+                {
+                    matrix[252 + i, desniRub + 1] = matrix[252 + i - 1, desniRub + 1] + matrix[252 + i - 1, desniRub] + matrix[252 + i, desniRub] + matrix[252 + i + 1, desniRub];
+                    number = matrix[252 + i, desniRub + 1];
+                    if (number > n)
+                    {
+                        Console.WriteLine(number);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                desniRub++;
+                //pogled odozgo
+                for (int i = vrati; i <= duljina; i++)
+                {
+                    matrix[gornjiRub + 1, 252 - i] = matrix[gornjiRub + 1, 252 - i + 1] + matrix[gornjiRub, 252 - i + 1] + matrix[gornjiRub, 252 - i] + matrix[gornjiRub, 252 - i - 1];
+                    number = matrix[gornjiRub + 1, 252 - i];
+                    if (number > n)
+                    {
+                        Console.WriteLine(number);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                gornjiRub++;
+                //pogled s lijeve
+                for (int i = vrati; i <= duljina; i++)
+                {
+                    matrix[252 - i, lijeviRub - 1] = matrix[252 - i + 1, lijeviRub - 1] + matrix[252 - i + 1, lijeviRub] + matrix[252 - i, lijeviRub] + matrix[252 - i - 1, lijeviRub];
+                    number = matrix[252 - i, lijeviRub - 1];
+                    if (number > n)
+                    {
+                        Console.WriteLine(number);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                lijeviRub--;
+                //pogled odozdo
+                for (int i = vrati; i <= duljina; i++)
+                {
+                    matrix[donjiRub - 1, 252 + i] = matrix[donjiRub - 1, 252 + i - 1] + matrix[donjiRub, 252 + i - 1] + matrix[donjiRub, 252 + i] + matrix[donjiRub, 252 + i + 1];
+                    number = matrix[donjiRub - 1, 252 + i];
+                    if (number > n)
+                    {
+                        Console.WriteLine(number);
+                        Console.ReadKey();
+                        return;
+                    }
+                }
+                donjiRub--;
+                duljina++;
+                vrati--;
+            }
         }
     }
 }
